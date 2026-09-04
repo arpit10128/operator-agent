9df219521d38368e385c16ae9e9a3a794d4cf4ca
arpit10128 <arpit10128@gmail.com>
2026-09-04 22:23:21 +0530

    routing and screen layout

---
+++ packages/cli/src/components/messages/user-message.tsx
@@ -0,0 +1,34 @@
+import { useTheme } from "../../providers/theme";
+import { EmptyBorder } from "../Border";
+
+type Props = {
+  message: string;
+};
+
+export function UserMessage({ message }: Props) {
+  const { colors } = useTheme();
+
+  return (
+    <box width="100%" alignItems="center">
+      <box
+        border={["left"]}
+        borderColor={colors.primary}
+        width="100%"
+        customBorderChars={{
+          ...EmptyBorder,
+          vertical: "\u2503",
+        }}
+      >
+        <box
+          justifyContent="center"
+          paddingX={2}
+          paddingY={1}
+          backgroundColor={colors.surface}
+          width="100%"
+        >
+          <text>{message}</text>
+        </box>
+      </box>
+    </box>
+  );
+}
